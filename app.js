9f33ec1d599ac9d5c7ef0294c797346c95a8550f
meekoder <meekorusdi@gmail.com>
2020-10-19 10:44:25 -0700

    completed step 3 -props

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// TODO
+/* Part 1 */
+// const GroceryList = () => (
+//   <ul>
+//     <li>Eggs</li>
+//     <li>Milk</li>
+//   </ul>
+// );
+//
+/* Part 2 */
+// const GroceryList = () => (
+//   <ul>
+//     <Eggs />
+//     <Milk />
+//   </ul>
+// );
+
+// const Eggs = () => ( 
+//   <li>Eggs</li>
+// );
+
+// const Milk = () => (
+//   <li>Milk</li>
+// );
+//
+/* Part 3 */
+const GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryListItem items={['Eggs', 'Milk']}/>
+  </div>
+);
+
+const GroceryListItem = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
